d96386944064e5d308e3d97c7fa938f0d6f03d9b
yoojung1 <jungsup.yoo@gmail.com>
2020-10-26 14:56:29 -0400

    Image config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
